b7a78e5ca61f2546e9e7585c7d6bded5adadb76e
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2024-06-05 06:16:54 +0200

    Adjust icon, name. Close application right after.

--- app/src/main/java/com/github/thg/youtubelauncher/MainActivity.kt
+++ app/src/main/java/com/github/thg/youtubelauncher/MainActivity.kt
@@ -3,16 +3,21 @@ package com.github.thg.youtubelauncher
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.Browser
 import androidx.activity.ComponentActivity
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        System.out.println("YouTubeLauncher")
         val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://youtube.com"))
+        browserIntent.putExtra(Browser.EXTRA_APPLICATION_ID, applicationContext.packageName);
         startActivity(browserIntent)
-        finish()
+        Handler(Looper.getMainLooper()).postDelayed({
+            finishAndRemoveTask()
+        }, 2000)
     }
 }
 
